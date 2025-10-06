8d138b8825af4837d72bc2ffdb6db0a9eec302b4
Rishabh Kumar <rsk1998200@gmail.com>
2025-10-07 00:20:40 +0530

    Added CSS file

--- 1. Inception/App.js
+++ 1. Inception/App.js
@@ -2,6 +2,7 @@
 // Syntax: React.createElement(type-of-tag, props-to-pass, children-to-pass-inside-the-created-tag)
 const heading = React.createElement(
   "h1",
+  // An object containing the attributes we want to pass to the created tag
   { id: "heading", custom: "customAttribute" },
   "Hello World! from React"
 );
@@ -9,7 +10,29 @@ const heading = React.createElement(
 // Prints the React element object
 console.log(heading);
 
+// Output:
+// {
+//     "type": "h1",
+//     "key": null,
+//     "ref": null,
+//     "props": {
+//         "id": "heading",
+//         "custom": "customAttribute",
+//         "children": "Hello World! from React"
+//     },
+//     "_owner": null,
+//     "_store": {}
+// }
+
+// React element is nothing but a JS object which describes the UI.
+
 // Create a root element (to interact with the DOM we use ReactDOM)
+// React does not know about the root element in the HTML file, so we need to tell ReactDOM about it.
+// and pass the root element to ReactDOM.createRoot() method.
+
+// Earlier we used React.createElement() to create a React element.
+// Now we will use ReactDOM.createRoot() to create a root element. Because ReactDOM is responsible for
+// interacting with the DOM, and we will add out elements to the DOM.
 // Syntax: ReactDOM.createRoot(document.getElementById("root") element)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
